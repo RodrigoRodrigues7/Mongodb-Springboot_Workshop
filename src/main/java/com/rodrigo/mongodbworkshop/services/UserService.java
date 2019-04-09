68f8a691d6157d06235c7734be454df7e7a09e33
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-04-09 11:05:39 -0300

    Obtendo usuarios por 'Id' e implementado as classes de para erros de objeto nao encontrado

--- src/main/java/com/rodrigo/mongodbworkshop/services/UserService.java
+++ src/main/java/com/rodrigo/mongodbworkshop/services/UserService.java
@@ -1,18 +1,25 @@
 package com.rodrigo.mongodbworkshop.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodrigo.mongodbworkshop.domain.User;
 import com.rodrigo.mongodbworkshop.repository.UserRepository;
+import com.rodrigo.mongodbworkshop.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	
 	@Autowired
 	private UserRepository repository;
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não Encontrado!"));
+	}
 	
 	public List<User> findAll() {
 		return repository.findAll();
