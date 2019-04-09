94635db7ab434033c9c98f340d685cc736783a2c
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-04-08 21:36:40 -0300

    First Commit

---
+++ src/test/java/com/rodrigo/mongodbworkshop/MongodbWorkshopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rodrigo.mongodbworkshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MongodbWorkshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
