33ae0b0c72c8e652640c0ff7f2223a5a9662dc53
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-04-10 18:36:00 -0300

    Alterada a projecao dos dados de 'User' no 'Post' com a classe 'AuthorDTO'

--- src/main/java/com/rodrigo/mongodbworkshop/config/Instatiation.java
+++ src/main/java/com/rodrigo/mongodbworkshop/config/Instatiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.rodrigo.mongodbworkshop.domain.Post;
 import com.rodrigo.mongodbworkshop.domain.User;
+import com.rodrigo.mongodbworkshop.dto.AuthorDTO;
 import com.rodrigo.mongodbworkshop.repository.PostRepository;
 import com.rodrigo.mongodbworkshop.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instatiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("15/02/2019"), "Bom Dia", "Acordei feliz hoje!", maria);
-		
 		userRepo.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("15/02/2019"), "Bom Dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepo.saveAll(Arrays.asList(post1, post2));
 	}
 	
