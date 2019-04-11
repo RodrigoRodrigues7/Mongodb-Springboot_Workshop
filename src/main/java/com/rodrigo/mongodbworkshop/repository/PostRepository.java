adf2eabbee885676567c4041e440481832860be6
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-04-11 12:02:32 -0300

    Consulta simples para buscar um 'Post' por titulo utilizando uma '@Query'

--- src/main/java/com/rodrigo/mongodbworkshop/repository/PostRepository.java
+++ src/main/java/com/rodrigo/mongodbworkshop/repository/PostRepository.java
@@ -3,14 +3,19 @@ package com.rodrigo.mongodbworkshop.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rodrigo.mongodbworkshop.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-	
-	//Esse método é um 'Query Method'
+
+	// Esse método é um 'Query Method'
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
+	// Esse método faz a mesma consulta que o método acima, mas usando uma '@Query'
+	@Query("{ 'title': {$regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
+
 }
